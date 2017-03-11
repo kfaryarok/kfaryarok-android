be55273c6759de2d2311aa7579dc9b8f8693a43c
Tbsc <me@tbscdev.xyz>
2017-03-11 14:24:05 +0200

    Coded the basics needed for fetching updates, all that's needed is to plug the URL in

--- app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/updates/UpdateFetcher.java
+++ app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/updates/UpdateFetcher.java
@@ -1,5 +1,14 @@
 package io.github.kfaryarok.kfaryarokapp.updates;
 
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Gets updates from a server, and saves the update into a String.
  * Doesn't parse the JSON, only gets it.
@@ -8,9 +17,54 @@ package io.github.kfaryarok.kfaryarokapp.updates;
  */
 public class UpdateFetcher {
 
+    // TODO actual data for fetching
+    public static final String UPDATE_SERVER = "";
+    public static final String UPDATE_PATH = "/update.json";
+
     public static String fetchUpdates() {
-        // TODO actually fetch something
-        return "";
+        try {
+            return getResponseFromHttpUrl(buildUrl());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
+    public static URL buildUrl() {
+        Uri uri = Uri.parse(UPDATE_SERVER).buildUpon()
+                .appendPath(UPDATE_PATH).build();
+
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
