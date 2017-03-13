f8c7556e5096f3ce464e8b0a0277638e102e23ba
Tbsc <me@tbscdev.xyz>
2017-03-13 10:54:58 +0200

    (nonfunctional) boot receiver for resettings alerts after restart

---
+++ app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/alerts/AlertBootReceiver.java
@@ -0,0 +1,17 @@
+package io.github.kfaryarok.kfaryarokapp.alerts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author tbsc on 11/03/2017
+ */
+public class AlertBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
