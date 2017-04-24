2b5f9c0603dbb735836e3c50f93de97f0d0b9f6e
Tbsc <danielben60@gmail.com>
2017-04-24 18:19:38 +0300

    Changed test objects to be more simple, and use the new UpdateImpl constructor

--- app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/util/TestUtil.java
+++ app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/util/TestUtil.java
@@ -26,16 +26,13 @@ public class TestUtil {
 
         JSONObject testJson = new JSONObject();
         try {
-            JSONObject globalUpdate1 = new JSONObject().put(UpdateParser.TEXT_STRING, "עדכון רגיל 1 \nל");
+            JSONObject globalUpdate1 = new JSONObject().put(UpdateParser.TEXT_STRING, "עדכון רגיל 1");
             JSONObject globalUpdate2 = new JSONObject().put(UpdateParser.TEXT_STRING, "עדכון רגיל 2");
             JSONArray globalUpdates = new JSONArray().put(globalUpdate1).put(globalUpdate2);
 
-            JSONObject update1 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל הרבה כיתות 1").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("ח4")/*.put("ט3").put("ט4").put("ז11").put("יא11").put("י6").put("י5").put("יב15").put("י11").put("ח11").put("ח5").put("ז1").put("ז2").put("ז3").put("ז4").put("ז5")*/);
+            JSONObject update1 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל 1").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("ח4"));
             JSONObject update2 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל 2").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("ט2").put("יא7"));
-            JSONObject update3 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל 3").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("ט5").put("ח4"));
-            JSONObject update4 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל 4").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("ט2"));
-            JSONObject update5 = new JSONObject().put(UpdateParser.TEXT_STRING, "רגיל 5").put(UpdateParser.CLASSES_ARRAY, new JSONArray().put("יב5"));
-            JSONArray updatesJson = new JSONArray().put(update1).put(update2).put(update3).put(update4).put(update5);
+            JSONArray updatesJson = new JSONArray().put(update1).put(update2);
 
             testJson.put(UpdateParser.GLOBAL_UPDATES_ARRAY, globalUpdates);
             testJson.put(UpdateParser.UPDATES_ARRAY, updatesJson);
@@ -51,6 +48,7 @@ public class TestUtil {
                 new UpdateImpl(new String[] {
                         "H4", "I10", "H5", "I10", "K1", "G5", "G4", "G3", "G2", "K11", "K10", "H3"
                 }, "private!"),
+                new UpdateImpl("J3", "כלום!"),
                 new UpdateImpl(new ClassesAffected("L69"), "test 1"),
                 new UpdateImpl("hi"),
                 new UpdateImpl("fua")
