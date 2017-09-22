d3814120b7c2b5779beb0b79c265f3c024823677
Tbsc <danielben60@gmail.com>
2017-09-22 13:58:41 +0300

    Backported more functional interfaces

--- app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/util/functional/FunctionalUtil.java
+++ app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/util/functional/FunctionalUtil.java
@@ -38,6 +38,22 @@ public class FunctionalUtil {
     }
 
     /**
+     * Returns a non-functional Function instance that always returns the given return value.
+     * @param returnValue What should the function return
+     * @param <T> Anything
+     * @param <R> Also anything
+     * @return An instance of {@link Function} that will do nothing and return the given return value
+     */
+    public static <T, R> Function<T, R> returnFunction(final R returnValue) {
+        return new Function<T, R>() {
+            @Override
+            public R apply(T t) {
+                return returnValue;
+            }
+        };
+    }
+
+    /**
      * Returns a non-functional Consumer instance. It does nothing.
      * @param <T> Anything
      * @return An instance of {@link Consumer} that will do nothing
@@ -47,6 +63,63 @@ public class FunctionalUtil {
             @Override
             public void accept(T t) {
                 // no-op
+            }
+        };
+    }
+
+    /**
+     * Returns a supplier that always supplies null
+     * @param <T> Anything
+     * @return An instance of {@link Supplier} that will always supply null
+     */
+    public static <T> Supplier<T> nullSupplier() {
+        return new Supplier<T>() {
+            @Override
+            public T get() {
+                return null;
+            }
+        };
+    }
+
+    /**
+     * Returns a supplier that always supplies the given object.
+     * @param object What should the supplier supply
+     * @param <T> Anything
+     * @return An instance of {@link Supplier} that will always supply the given object
+     */
+    public static <T> Supplier<T> objectSupplier(final T object) {
+        return new Supplier<T>() {
+            @Override
+            public T get() {
+                return object;
+            }
+        };
+    }
+
+    /**
+     * Returns a predicate that will always test false.
+     * @param <T> Anything
+     * @return An instance of {@link Predicate} that will always test false
+     */
+    public static <T> Predicate<T> falsePredicate() {
+        return new Predicate<T>() {
+            @Override
+            public boolean test(T t) {
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Returns a predicate that will always test true.
+     * @param <T> Anything
+     * @return An instance of {@link Predicate} that will always test true
+     */
+    public static <T> Predicate<T> truePredicate() {
+        return new Predicate<T>() {
+            @Override
+            public boolean test(T t) {
+                return true;
             }
         };
     }
---
+++ app/app/src/main/java/io/github/kfaryarok/kfaryarokapp/util/functional/Predicate.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of kfaryarok-android.
+ *
+ * kfaryarok-android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kfaryarok-android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kfaryarok-android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.kfaryarok.kfaryarokapp.util.functional;
+
+/**
+ * @author tbsc on 22/09/2017
+ */
+public interface Predicate<T> {
+
+    /**
+     * Evaluates this predicate on the given argument.
+     *
+     * @param t the input argument
+     * @return {@code true} if the input argument matches the predicate,
+     * otherwise {@code false}
+     */
+    boolean test(T t);
+
+}
